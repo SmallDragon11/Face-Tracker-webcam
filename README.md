63dc2b7bc8196ad5f2a185fa29bed84b5f3972e5
Small Dragon <mick20001108@gmail.com>
2022-05-27 01:51:01 +0800

    FaceTrackWebCam_YOLOv4tiny

---
+++ README.md
@@ -0,0 +1,23 @@
+# 追蹤人臉 WebCam (YOLOv4_tiny + Arduino servo)
+
+## reference: https://create.arduino.cc/projecthub/shubhamsantosh99/face-tracker-using-opencv-and-arduino-55412e
+
+PC端使用python OpenCV匯入YOLOv4_tiny模型偵測人臉，再透過pyserial套件與arduino溝通。
+
+本篇使用Arduino Uno來控制兩顆servo馬達，自動調整鏡頭的角度，讓人臉盡量維持在鏡頭中心
+
+原作者使用Harr cascade of classifiers 來做人臉偵測 -> 速度慢，精確度不高
+
+所以我改使用YOLOv4_tiny來偵測人臉，達到即時偵測的性能，且精確度明顯提升
+
+# 訓練
+
+YOLOv4_tiny: 利用darknet以資料集WIDER FACE (http://shuoyang1213.me/WIDERFACE/) 進行訓練。可參考: https://medium.com/ching-i/yolo-c49f70241aa7
+
+
+
+# 待改進
+
+yolo 使用GPU可以大大提升FPS
+
+但...openCV只能用CPU，如果要使用GPU必須安裝額外的擴充套件，在windows環境下並不好裝(我在win10裝darknet的時候差點過世)
---
+++ python_script.py
@@ -0,0 +1,62 @@
+#Face tracker using OpenCV and Arduino
+#by Shubham Santosh
+
+import cv2
+import serial,time
+import argparse
+
+parser = argparse.ArgumentParser(description='Code for FaceTrackCam.')
+parser.add_argument('--camera', '-cam', help='Camera divide number.', type=int, default=0)
+parser.add_argument('--comPort', '-com', help='Arduino COM port number.', type=str, default='com3')
+args = parser.parse_args()
+
+# face_cascade= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap=cv2.VideoCapture(args.camera)
+#fourcc= cv2.VideoWriter_fourcc(*'XVID')
+ArduinoSerial=serial.Serial(args.comPort,9600,timeout=0.1)
+#out= cv2.VideoWriter('face detection4.avi',fourcc,20.0,(640,480))
+time.sleep(1)
+
+# with open('face.names', 'r') as f:
+#     classes = f.read().splitlines()
+
+net = cv2.dnn.readNetFromDarknet('yolov4-tiny-obj.cfg', 'yolov4-tiny-obj_last.weights')
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(scale=1 / 255, size=(416, 416), swapRB=True)
+
+while cap.isOpened():
+    ret, frame= cap.read()
+    frame=cv2.flip(frame,1)  #mirror the image
+    # frame = cv2.resize(frame, (416, 416))
+    #print(frame.shape)
+    # gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    # faces= face_cascade.detectMultiScale(gray,1.1,6)  #detect the face
+    classIds, scores, faces = model.detect(frame, confThreshold=0.6, nmsThreshold=0.4)
+
+    for x,y,w,h in faces:
+        #sending coordinates to Arduino
+        string='X{0:d}Y{1:d}'.format((x+w//2),(y+h//2))
+        print(string)
+        ArduinoSerial.write(string.encode('utf-8'))
+        #plot the center of the face
+        cv2.circle(frame,(x+w//2,y+h//2),2,(0,255,0),2)
+        #plot the roi
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
+    #plot the squared region in the center of the screen
+    cv2.rectangle(frame,(640//2-30,480//2-30),
+                 (640//2+30,480//2+30),
+                  (255,255,255),3)
+    #out.write(frame)
+    cv2.imshow('img',frame)
+    #cv2.imwrite('output_img.jpg',frame)
+    
+    # for testing purpose
+    # read= str(ArduinoSerial.readline(ArduinoSerial.inWaiting()))
+    # time.sleep(0.05)
+    # print('data from arduino:'+read)
+    
+    # press q to Quit
+    if cv2.waitKey(10)&0xFF== ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
